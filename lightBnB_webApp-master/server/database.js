7eacedd324ade6f263d470a9c4979de207695a45
linglingliuTO <linglingliu.to@gmail.com>
2022-10-05 23:46:42 +0000

    add property

--- lightBnB_webApp-master/server/database.js
+++ lightBnB_webApp-master/server/database.js
@@ -90,7 +90,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   return pool
   .query(`SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating FROM reservations JOIN properties ON reservations.property_id = properties.id JOIN property_reviews ON properties.id = property_reviews.property_id WHERE reservations.guest_id = $1 GROUP BY properties.id, reservations.id ORDER BY reservations.start_date LIMIT $2`, [guest_id, limit])
 .then ((result) => {
-  console.log("result_rows", result.rows);
   return result.rows;
 
 })
@@ -181,14 +180,15 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-/*const addProperty = function(property) {
+
+
+const addProperty = function(property) {
 
 
   return pool 
-  .query('INSERT INTO properties (owner_id, title: string, description: string,thumbnail_photo_url: string, cover_photo_url: string, cost_per_night: string,street: string, city: string, province: string, post_code: string, country: string, parking_spaces: int, number_of_bathrooms: int,
-    number_of_bedrooms: int) VALUES ($1,$2,$3)', [user.name, user.email, user.password])
+  .query('INSERT INTO properties (owner_id, title, description,thumbnail_photo_url, cover_photo_url, cost_per_night,street, city, province, post_code, country, parking_spaces, number_of_bathrooms,number_of_bedrooms) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *', [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night,property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms,property.number_of_bedrooms])
   .then ((result) => {
-    returning * ;
+    return result.rows[0]
   })
   .catch((err) => {
     console.log(err.message);
@@ -197,8 +197,5 @@ exports.getAllProperties = getAllProperties;
 
 
 
-
-
 }
 exports.addProperty = addProperty;
-*/
